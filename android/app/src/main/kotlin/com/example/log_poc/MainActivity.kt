e4d2bd2c63de1fb02f4d560f9434a81104886115
Sahlet <sahlet.official@gmail.com>
2024-08-08 18:15:51 +0200

    remove many

--- android/app/src/main/kotlin/com/example/log_poc/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.wayarts.log-poc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- log_poc/lib/main.dart
+++ log_poc/lib/main.dart
@@ -48,51 +48,52 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("збс"),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                labelText: 'Enter number of secconds',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            ListView(
-              children: _timersViews,
-            ),
-            FloatingActionButton(
-              onPressed: _startTimer,
-              tooltip: 'startTimer',
-              child: const Icon(Icons.start),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addTimer,
-        tooltip: 'addTimer',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return ListView();
+    // // This method is rerun every time setState is called, for instance as done
+    // // by the _incrementCounter method above.
+    // //
+    // // The Flutter framework has been optimized to make rerunning build methods
+    // // fast, so that you can just rebuild anything that needs updating rather
+    // // than having to individually change instances of widgets.
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     // TRY THIS: Try changing the color here to a specific color (to
+    //     // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+    //     // change color while the other colors stay the same.
+    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    //     // Here we take the value from the MyHomePage object that was created by
+    //     // the App.build method, and use it to set our appbar title.
+    //     title: const Text("збс"),
+    //   ),
+    //   body: Center(
+    //     // Center is a layout widget. It takes a single child and positions it
+    //     // in the middle of the parent.
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         TextField(
+    //           controller: _controller,
+    //           decoration: const InputDecoration(
+    //             labelText: 'Enter number of secconds',
+    //             border: OutlineInputBorder(),
+    //           ),
+    //         ),
+    //         ListView(
+    //           children: _timersViews,
+    //         ),
+    //         FloatingActionButton(
+    //           onPressed: _startTimer,
+    //           tooltip: 'startTimer',
+    //           child: const Icon(Icons.start),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _addTimer,
+    //     tooltip: 'addTimer',
+    //     child: const Icon(Icons.add),
+    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+    // );
   }
 }
